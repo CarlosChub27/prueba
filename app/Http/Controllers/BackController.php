5e567f7b2d545cb7005d22393f1373f925e31942
CarlosChub <cchub@researchmobile.co>
2024-07-04 09:13:20 -0600

    listo

--- app/Http/Controllers/BackController.php
+++ app/Http/Controllers/BackController.php
@@ -15,18 +15,21 @@ class BackController extends Controller
 
     public function handlePost(Request $request)
     {
-
         // Obtener todos los datos de la solicitud (form-data, x-www-form-urlencoded, json)
-        $allData = $request->all();
+        $id = $request->query('id');
+        $type = $request->query('type');
+        $allData = $request->getContent();
         Log::info('datos de la solicitud: ' . json_encode($allData));
-
+        Log::info('id: ' . $id);
+        Log::info('type ' . $type);
+        
+        
         // Procesar los datos
         if (!empty($allData)) {
             $response = ['success' => true, 'message' => 'Datos form recibidos', 'data' => $allData];
         } else {
             $response = ['success' => false, 'message' => 'No se recibieron datos'];
         }
-
         return response()->json($response, 200);
     }
 }
